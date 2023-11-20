6c35205bd672b27336c1ac234302fc436107c62a
glebsolovyov <solovyovgleb1@gmail.com>
2023-11-21 00:20:23 +0300

    add,delete db logic

--- lib/data/Database.dart
+++ lib/data/Database.dart
@@ -22,10 +22,11 @@ class DBProvider {
     _database = await initDB();
     return _database;
   }
-  
-    initDB() async {
+
+  initDB() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join("/Users/glebsolovyov/flutter_projects/weather_app/weather_app.sqlite");
+    String path = join(
+        "/Users/glebsolovyov/flutter_projects/weather_app/weather_app.sqlite");
     return await openDatabase(path, version: 1, onOpen: (db) {},
         onCreate: (Database db, int version) async {
       await db.execute("CREATE TABLE City ("
@@ -37,7 +38,7 @@ class DBProvider {
 
   newCity(String name) async {
     final db = await database;
-    
+
     var table = await db?.rawQuery("SELECT MAX(id)+1 as id FROM City");
     Object? id = table!.first["id"];
     Object? isSelected = 0;
@@ -52,13 +53,47 @@ class DBProvider {
   Future<List<City>> getAllCities() async {
     final db = await database;
     var res = await db?.query("City");
-    List<City> list = res!.isNotEmpty ? res.map((c) => City.fromJson(c)).toList() : [];
+    List<City> list =
+        res!.isNotEmpty ? res.map((c) => City.fromJson(c)).toList() : [];
     return list;
+  }
+
+  Future<bool> isContainsCity(String cityName) async {
+    final db = await database;
+    var res = await db?.query("City");
+    if (res!.isNotEmpty) {
+      for (var item in res) {
+        String name = item["name"]!.toString().toLowerCase();
+        if (name == cityName.toLowerCase()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  Future<int> getCityIdByName(String cityName) async {
+    final db = await database;
+    var res = await db?.query("City");
+    if (res!.isNotEmpty) {
+      for (var item in res) {
+        String name = item["name"]!.toString().toLowerCase();
+        if (name == cityName.toLowerCase()) {
+          return int.parse(item["id"].toString());
+        }
+      }
+    }
+    return 0;
   }
 
   Future<City?> getSelectedCity() async {
     final db = await database;
     var res = await db?.query("City", where: "isSelected = ?", whereArgs: [1]);
     return res!.isNotEmpty ? City.fromJson(res.first) : null;
+  }
+
+  deleteCity(int id) async {
+    final db = await database;
+    return db!.delete("City", where: "id = ?", whereArgs: [id]);
   }
 }
--- lib/widget/pages/cities_page.dart
+++ lib/widget/pages/cities_page.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/data/database.dart';
 import 'package:weather_app/data/weather_data_provider.dart';
 
-import '../../data/result.dart';
-import '../../models/city.dart';
-
 class CitiesPage extends StatefulWidget {
   const CitiesPage({super.key});
 
@@ -14,55 +11,132 @@ class CitiesPage extends StatefulWidget {
 
 class _CitiesPage extends State<CitiesPage> with TickerProviderStateMixin {
   String _newCityName = "";
-  bool correctCity = true;
+  bool correctCity = false;
+  bool error = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: TextFormField(
-            decoration: InputDecoration(
-                errorText: !correctCity ? "Город не найден" : null),
-            onFieldSubmitted: (text) async {
-              try {
-                await fetchWeather(text);
-                DBProvider.db.newCity(text);
-              } on Exception catch (_) {
-                setState(() {
-                  correctCity = false;
-                });
-              }
-            },
-          ),
-        ),
-        body: FutureBuilder(
-            future: DBProvider.db.getAllCities(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: ((context, index) {
-                      return FutureBuilder<Map<String, List>>(
-                          future: fetchWeather(snapshot.data![index].name),
-                          builder: (context, citySnapshot) {
-                            if (citySnapshot.hasData) {
-                              final hourlyData =
-                                  citySnapshot.data!["hourly"]![0];
-                              return Card(
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+              title: Text("Города"),
+              bottom: const TabBar(
+                tabs: <Widget>[Tab(text: "Список"), Tab(text: "Поиск")],
+              )),
+          body: TabBarView(
+            children: <Widget>[
+              FutureBuilder(
+                  future: DBProvider.db.getAllCities(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: ((context, index) {
+                            return FutureBuilder<Map<String, List>>(
+                                future:
+                                    fetchWeather(snapshot.data![index].name),
+                                builder: (context, citySnapshot) {
+                                  if (citySnapshot.hasData) {
+                                    final hourlyData =
+                                        citySnapshot.data!["hourly"]![0];
+                                    return Card(
+                                      child: Row(
+                                        children: [
+                                          Text(snapshot.data![index].name),
+                                          Text(hourlyData.temp.toString())
+                                        ],
+                                      ),
+                                    );
+                                  } else {
+                                    return Text("Нет инф");
+                                  }
+                                });
+                          }));
+                    } else {
+                      return Text("Города не добавлены");
+                    }
+                  }),
+              Column(children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      errorText: error ? "Город не найден" : null),
+                  onFieldSubmitted: (text) async {
+                    try {
+                      await fetchWeather(text);
+                      setState(() {
+                        correctCity = true;
+                        error = false;
+                        _newCityName = text;
+                      });
+                    } on Exception catch (_) {
+                      setState(() {
+                        error = true;
+                        correctCity = false;
+                      });
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 100,
+                  child: Visibility(
+                    visible: correctCity,
+                    child: FutureBuilder(
+                        future: fetchWeather(_newCityName),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final hourlyData = snapshot.data!["hourly"]![0];
+                            return Card(
                                 child: Row(
-                                  children: [
-                                    Text(snapshot.data![index].name),
-                                    Text(hourlyData.temp.toString())
-                                  ],
-                                ),
-                              );
-                            } else {
-                              return Text("Нет инф");
-                            }
-                          });
-                    }));
-              } else {
-                return Text("Города не добавлены");
-              }
-            }));
+                              children: [
+                                Text(hourlyData.city.toString()),
+                                Text(hourlyData.temp.toString()),
+                                FutureBuilder(
+                                    future: DBProvider.db
+                                        .isContainsCity(_newCityName),
+                                    builder: (context, citySnapshot) {
+                                      if (citySnapshot.hasData) {
+                                        return FutureBuilder(
+                                            future: DBProvider.db
+                                                .getCityIdByName(_newCityName),
+                                            builder: (context, cityIdSnapshot) {
+                                              if (cityIdSnapshot.hasData) {
+                                                return !citySnapshot.data!
+                                                    ? ElevatedButton(
+                                                        onPressed: () {
+                                                          DBProvider.db.newCity(
+                                                              _newCityName);
+                                                        },
+                                                        child: Icon(Icons.add))
+                                                    : ElevatedButton(
+                                                        onPressed: () {
+                                                          DBProvider.db
+                                                              .deleteCity(
+                                                                  cityIdSnapshot
+                                                                      .data!);
+                                                        },
+                                                        child:
+                                                            Icon(Icons.remove));
+                                              } else {
+                                                return Text("Ошибка");
+                                              }
+                                            });
+                                      } else {
+                                        return Text("Ошибка");
+                                      }
+                                    })
+                              ],
+                            ));
+                          } else {
+                            return Text("Загрузка");
+                          }
+                        }),
+                  ),
+                )
+              ]),
+            ],
+          )),
+    );
   }
 }
